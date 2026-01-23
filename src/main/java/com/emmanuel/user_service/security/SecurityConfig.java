38a26d4ece6ef0922fd0b8f5e25b4de1121138a7
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2026-01-23 21:08:18 +0000

    feat: add Prometheus metrics and actuator endpoints configuration
    Signed-off-by: Emmanuel Mireku Omari <omariemmanuel91@gmail.com>

--- src/main/java/com/emmanuel/user_service/security/SecurityConfig.java
+++ src/main/java/com/emmanuel/user_service/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     return http.csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(
             auth ->
-                auth.requestMatchers("/auth/**")
+                auth.requestMatchers("/auth/**"," /swagger-ui/**", "/v3/api-docs/**","/actuator/**")
                     .permitAll()
                     .requestMatchers("/admin/**")
                     .hasRole(Roles.ADMIN)
